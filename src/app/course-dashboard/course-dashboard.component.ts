57c2f92b57f63a0e945c308160051aab0148162f
Aron <aronalecdgr@gmail.com>
2025-05-28 11:07:57 +0800

    initial commit course-dashboard

---
+++ src/app/course-dashboard/course-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-dashboard',
+  imports: [],
+  templateUrl: './course-dashboard.component.html',
+  styleUrl: './course-dashboard.component.css'
+})
+export class CourseDashboardComponent {
+
+}
